f7d3a5ba74b9e0cc883695e4a19eee25169f3fca
Prithvisagar Shivaraman <prithvisagar.shivaraman@gmail.com>
2021-08-29 22:58:33 -0400

    Reduced blocking nature of legendreutil.

--- src/MathTools/LegendreUtil.cpp
+++ src/MathTools/LegendreUtil.cpp
@@ -13,35 +13,38 @@ using namespace AviationCalcUtil::MathTools;
 std::mutex LegendreUtil::legendreCacheLock;
 
 std::shared_ptr<const Polynomial> LegendreUtil::legendrePolynomial(int n) {
-    vector<double> coeffs(n + 1);
+	vector<double> coeffs(n + 1);
 
-    for (int  m = 0; m <= n / 2; m++){
-        coeffs.at(n - 2 * m) = pow(-1, m) / pow(2, n);
-        coeffs.at(n - 2 * m) *= factorialRatio(2 * n - 2 * m, n - m) / (double)(MathUtil::factorial(m) * MathUtil::factorial(n - 2 * m));
-    }
+	for (int m = 0; m <= n / 2; m++) {
+		coeffs.at(n - 2 * m) = pow(-1, m) / pow(2, n);
+		coeffs.at(n - 2 * m) *= factorialRatio(2 * n - 2 * m, n - m) / (double)((long long)MathUtil::factorial(m) * (long long) MathUtil::factorial(n - 2 * m));
+	}
 
-    return std::make_shared<Polynomial>(coeffs);
+	return std::make_shared<Polynomial>(coeffs);
 }
 
 double LegendreUtil::factorialRatio(int n, int m) {
-    if (n > m){
-        return n * factorialRatio(n - 1, m);
-    }
+	if (n > m) {
+		return n * factorialRatio(n - 1, m);
+	}
 
-    return 1;
+	return 1;
 }
 
 double LegendreUtil::legendreFunction(int n, int m, double x) {
-    std::shared_ptr<Polynomial> p;
-    tuple<int, int> key = make_tuple(n, m);
+	std::shared_ptr<Polynomial> p;
+	tuple<int, int> key = make_tuple(n, m);
 
-    const std::lock_guard<std::mutex> gd_lock(legendreCacheLock);
-    try {
-        p = legendreCache.at(key);
-    } catch (const std::out_of_range&){
-        p = legendrePolynomial(n)->derivative(m);
-        legendreCache.insert(std::make_pair(key, p));
-    }
+	try {
+		const std::lock_guard<std::mutex> gd_lock(legendreCacheLock);
+		p = legendreCache.at(key);
+	}
+	catch (const std::out_of_range&) {
+		p = legendrePolynomial(n)->derivative(m);
 
-    return pow(1 - pow(x, 2), m / 2.0) * p->evaluate(x);
+		const std::lock_guard<std::mutex> gd_lock(legendreCacheLock);
+		legendreCache.insert(std::make_pair(key, p));
+	}
+
+	return pow(1 - pow(x, 2), m / 2.0) * p->evaluate(x);
 }
