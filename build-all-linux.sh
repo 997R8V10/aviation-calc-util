0d84c642debaa27a79a8f9e8f6e62be3561f29af
Prithvisagar Shivaraman <prithvisagar.shivaraman@gmail.com>
2021-09-13 21:19:03 -0400

    Fixed linux build script

--- build-all-linux.sh
+++ build-all-linux.sh
@@ -28,8 +28,10 @@ echo ""
 mkdir -p build/linux/x86/debug
 echo "Installing Conan Packages"
 conan install . -if build/linux/x86/debug -s arch=x86 -s build_type=Debug --build=missing
-echo "Building with Conan"
-conan build . -bf build/linux/x86/debug
+echo "Generating CMake Files"
+cmake -S . -B build/linux/x86/debug -DCMAKE_BUILD_TYPE=Debug -A x86
+echo "Building with CMake"
+cmake --build build/linux/x86/debug --target aviationcalc
 echo ""
 
 echo "\\--------------------------------------------------------------\\"
@@ -39,9 +41,11 @@ echo ""
 
 mkdir -p build/linux/x86/release
 echo "Installing Conan Packages"
-conan install . -if build/linux/x86/release -s arch=x86 -s build_type=Debug --build=missing
-echo "Building with Conan"
-conan build . -bf build/linux/x86/release
+conan install . -if build/linux/x86/release -s arch=x86 -s build_type=Release --build=missing
+echo "Generating CMake Files"
+cmake -S . -B build/linux/x86/release -DCMAKE_BUILD_TYPE=Release -A x86
+echo "Building with CMake"
+cmake --build build/linux/x86/release --target aviationcalc
 echo ""
 
 echo "\\--------------------------------------------------------------\\"
@@ -51,9 +55,11 @@ echo ""
 
 mkdir -p build/linux/x64/debug
 echo "Installing Conan Packages"
-conan install . -if build/linux/x86/release -s arch=x86 -s build_type=Debug --build=missing
-echo "Building with Conan"
-conan build . -bf build/linux/x86/release
+conan install . -if build/linux/x64/debug -s arch=x64 -s build_type=Debug --build=missing
+echo "Generating CMake Files"
+cmake -S . -B build/linux/x64/debug -DCMAKE_BUILD_TYPE=Debug -A x64
+echo "Building with CMake"
+cmake --build build/linux/x64/debug --target aviationcalc
 echo ""
 
 echo "\\--------------------------------------------------------------\\"
@@ -63,7 +69,9 @@ echo ""
 
 mkdir -p build/linux/x64/release
 echo "Installing Conan Packages"
-conan install . -if build/linux/x86/release -s arch=x86 -s build_type=Debug --build=missing
-echo "Building with Conan"
-conan build . -bf build/linux/x86/release
+conan install . -if build/linux/x64/release -s arch=x86 -s build_type=Release --build=missing
+echo "Generating CMake Files"
+cmake -S . -B build/linux/x64/release -DCMAKE_BUILD_TYPE=Release -A x64
+echo "Building with CMake"
+cmake --build build/linux/x64/release --target aviationcalc
 echo ""
