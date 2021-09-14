ee28061b6fcb915cf505048c612fcc76864b15f5
Caspian Merlin <caspian@merlins.online>
2021-09-14 02:32:48 +0100

    Removed -A parameter from cmake commands and corrected x64 to x86_64

--- build-all-linux.sh
+++ build-all-linux.sh
@@ -29,7 +29,7 @@ mkdir -p build/linux/x86/debug
 echo "Installing Conan Packages"
 conan install . -if build/linux/x86/debug -s arch=x86 -s build_type=Debug --build=missing
 echo "Generating CMake Files"
-cmake -S . -B build/linux/x86/debug -DCMAKE_BUILD_TYPE=Debug -A x86
+cmake -S . -B build/linux/x86/debug -DCMAKE_BUILD_TYPE=Debug
 echo "Building with CMake"
 cmake --build build/linux/x86/debug --target aviationcalc
 echo ""
@@ -43,7 +43,7 @@ mkdir -p build/linux/x86/release
 echo "Installing Conan Packages"
 conan install . -if build/linux/x86/release -s arch=x86 -s build_type=Release --build=missing
 echo "Generating CMake Files"
-cmake -S . -B build/linux/x86/release -DCMAKE_BUILD_TYPE=Release -A x86
+cmake -S . -B build/linux/x86/release -DCMAKE_BUILD_TYPE=Release
 echo "Building with CMake"
 cmake --build build/linux/x86/release --target aviationcalc
 echo ""
@@ -55,9 +55,9 @@ echo ""
 
 mkdir -p build/linux/x64/debug
 echo "Installing Conan Packages"
-conan install . -if build/linux/x64/debug -s arch=x64 -s build_type=Debug --build=missing
+conan install . -if build/linux/x64/debug -s arch=x86_64 -s build_type=Debug --build=missing
 echo "Generating CMake Files"
-cmake -S . -B build/linux/x64/debug -DCMAKE_BUILD_TYPE=Debug -A x64
+cmake -S . -B build/linux/x64/debug -DCMAKE_BUILD_TYPE=Debug
 echo "Building with CMake"
 cmake --build build/linux/x64/debug --target aviationcalc
 echo ""
@@ -69,9 +69,9 @@ echo ""
 
 mkdir -p build/linux/x64/release
 echo "Installing Conan Packages"
-conan install . -if build/linux/x64/release -s arch=x86 -s build_type=Release --build=missing
+conan install . -if build/linux/x64/release -s arch=x86_64 -s build_type=Release --build=missing
 echo "Generating CMake Files"
-cmake -S . -B build/linux/x64/release -DCMAKE_BUILD_TYPE=Release -A x64
+cmake -S . -B build/linux/x64/release -DCMAKE_BUILD_TYPE=Release
 echo "Building with CMake"
 cmake --build build/linux/x64/release --target aviationcalc
 echo ""
