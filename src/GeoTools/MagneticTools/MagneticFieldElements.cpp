a306f1af295a56c755c1beaab50adc7f9afebf5b
Caspian Merlin <caspian@merlins.online>
2021-09-15 04:21:39 +0100

    Done marshalling for MagneticFieldElements

--- src/GeoTools/MagneticTools/MagneticFieldElements.cpp
+++ src/GeoTools/MagneticTools/MagneticFieldElements.cpp
@@ -71,3 +71,118 @@ double MagneticFieldElements::getIncl() const {
 void MagneticFieldElements::setIncl(double newIncl) {
     incl = newIncl;
 }
+
+MagneticFieldElements *CreateMagneticFieldElements() {
+    return new MagneticFieldElements();
+}
+
+double MagneticFieldElementsGetX(MagneticFieldElements *ptr) {
+    if (ptr != nullptr)
+    {
+        return ptr->getX();
+    }
+    return -1;
+}
+
+void MagneticFieldElementsSetX(MagneticFieldElements *ptr, double newX) {
+    if (ptr != nullptr)
+    {
+        ptr->setX(newX);
+    }
+}
+
+double MagneticFieldElementsGetY(MagneticFieldElements *ptr) {
+    if (ptr != nullptr)
+    {
+        return ptr->getY();
+    }
+    return -1;
+}
+
+void MagneticFieldElementsSetY(MagneticFieldElements *ptr, double newY) {
+    if (ptr != nullptr)
+    {
+        ptr->setY(newY);
+    }
+}
+
+double MagneticFieldElementsGetZ(MagneticFieldElements *ptr) {
+    if (ptr != nullptr)
+    {
+        return ptr->getZ();
+    }
+    return -1;
+}
+
+void MagneticFieldElementsSetZ(MagneticFieldElements *ptr, double newZ) {
+    if (ptr != nullptr)
+    {
+        ptr->setZ(newZ);
+    }
+}
+
+double MagneticFieldElementsGetH(MagneticFieldElements *ptr) {
+    if (ptr != nullptr)
+    {
+        return ptr->getH();
+    }
+    return -1;
+}
+
+void MagneticFieldElementsSetH(MagneticFieldElements *ptr, double newH) {
+    if (ptr != nullptr)
+    {
+        ptr->setH(newH);
+    }
+}
+
+double MagneticFieldElementsGetF(MagneticFieldElements *ptr) {
+    if (ptr != nullptr)
+    {
+        return ptr->getF();
+    }
+    return -1;
+}
+
+void MagneticFieldElementsSetF(MagneticFieldElements *ptr, double newF) {
+    if (ptr != nullptr)
+    {
+        ptr->setF(newF);
+    }
+}
+
+double MagneticFieldElementsGetDecl(MagneticFieldElements *ptr) {
+    if (ptr != nullptr)
+    {
+        return ptr->getDecl();
+    }
+    return -1;
+}
+
+void MagneticFieldElementsSetDecl(MagneticFieldElements *ptr, double newDecl) {
+    if (ptr != nullptr)
+    {
+        ptr->setDecl(newDecl);
+    }
+}
+
+double MagneticFieldElementsGetIncl(MagneticFieldElements *ptr) {
+    if (ptr != nullptr)
+    {
+        return ptr->getIncl();
+    }
+    return -1;
+}
+
+void MagneticFieldElementsSetIncl(MagneticFieldElements *ptr, double newIncl) {
+    if (ptr != nullptr)
+    {
+        ptr->setIncl(newIncl);
+    }
+}
+
+void DisposeMagneticElements(MagneticFieldElements *ptr) {
+    if (ptr != nullptr) {
+        delete ptr;
+        ptr = nullptr;
+}
